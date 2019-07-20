c141d096864d7ee53693fb7fa44e38f7b548fb5d
codearoni <jake88brown@gmail.com>
2019-07-20 14:32:55 -0500

    Added additional tests

--- test/index.test.js
+++ test/index.test.js
@@ -35,6 +35,17 @@ describe('asyncForEach', () => {
         expect(err).to.be.an('error');
     });
 
+    it('Should return correctly on standard lexical function', async () => {
+
+        const mult = 10;
+
+        const fnReturn = await asyncForEach([1,2,3,4], (num) => {
+            return num * mult;
+        });        
+
+        expect(fnReturn).to.include.ordered.members([1 * mult, 2 * mult, 3 * mult, 4 * mult]);
+    });
+
     it('Should multiply all values by 2', async () => {
 
         const numArr = [2,4,6,8];
@@ -53,5 +64,25 @@ describe('asyncForEach', () => {
 
         expect(multipliedNums.length).to.equal(numArr.length);
         expect(expectedArr).to.include.ordered.members(multipliedNums);
+    });
+
+    it('Should provide the correct index for each iteration', async () => {
+
+        const strArr = ['a', 's', 'd', 'f'];
+
+        await asyncForEach(strArr, async (str, i) => {
+            expect(str).to.equal(strArr[i]);
+        });
+    });
+
+    it('Should provide the array for each iteration', async () => {
+    
+        const strArr = ['a', 's', 'd', 'f'];
+
+        await asyncForEach(strArr, async (str, i, array) => {
+            expect(str).to.equal(strArr[i]);
+            expect(str).to.equal(array[i]);
+            expect(array.length).to.equal(strArr.length);
+        });
     });
 });
